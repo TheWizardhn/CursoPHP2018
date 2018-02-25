40ce760fa96b2053ee6b7d74d549f0b4b95c7c14
Elvin R. Castellanos <elvin.castellanos@unitec.edu>
2018-02-24 23:40:45 -0600

    Create ejemploPost1.php
    El código necesario para consultar datos.

---
+++ ejemploPost1.php
@@ -0,0 +1,11 @@
+<?php 
+$diametro = $_POST['diam'];
+$altura = $_POST['altu'];
+$radio = $diametro/2;
+
+$Pi = 3.141593;
+
+$volumen = $Pi * $radio * $radio* $altura;
+
+echo "<br> &nbsp; El volumen del cilindro es de ". $volumen. "metros cúbicos";
+?>
